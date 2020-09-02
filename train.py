bec32c570865322ddd809adbc6b79cde02b69df1
Kaliostrogoblin <kaliostrogoblin3@gmail.com>
2020-09-03 01:12:40 +0300

    add absl flags

--- train.py
+++ train.py
@@ -3,11 +3,22 @@ import gin
 import torch
 import torch.nn as nn
 
+from absl import flags
+from absl import app
 from ariadne.core.training import TrainModel
 
-logging.basicConfig()
-LOGGER = logging.getLogger('ariadne')
+FLAGS = flags.FLAGS
+flags.DEFINE_string(
+    name='config', default=None,
+    help='Path to the config file to use.'
+)
+flags.DEFINE_enum(
+    name='log', default='INFO',
+    enum_values=['INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'],
+    help='Level of logging'
+)
 
+LOGGER = logging.getLogger('ariadne.train')
 
 @gin.configurable
 def experiment(model,
@@ -15,7 +26,7 @@ def experiment(model,
                metrics,
                optimizer):
     LOGGER.info('Create model for training')
-    # create model for training
+    # create model for trainingimport logging
     model = TrainModel(
         model=model,
         criterion=criterion,
@@ -25,11 +36,12 @@ def experiment(model,
     LOGGER.info(model)
 
 
-def main():
-    gin.parse_config(open('resources/gin/tracknet_v2_train.cfg'))
-    LOGGER.setLevel('INFO')
+def main(argv):
+    del argv
+    gin.parse_config(open(FLAGS.config))
+    LOGGER.setLevel(FLAGS.log)
     experiment()
 
 
 if __name__ == '__main__':
-    main()
+    app.run(main)
