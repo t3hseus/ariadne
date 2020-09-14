0ef8c8c40f9d65252a566ca33fee258ca7f3c6ad
Pavel Goncharov <pgonchar@space15.hydra.local>
2020-09-15 00:11:02 +0300

    add scripts for hydra

---
+++ scripts/hydra/hydra_gpu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+
+lscpu
+nvidia-smi
+conda env update --file environment_gpu.yml --name ariadne_gpu
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate ariadne_gpu
+"$@"
