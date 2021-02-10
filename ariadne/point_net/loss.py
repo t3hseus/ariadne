d3474e5f7ca64a82877d0fd4764ab58e1859c4fb
Egor Schavelev <egor5700@gmail.com>
2021-02-10 16:57:54 +0300

    modelv2 trains

--- ariadne/point_net/loss.py
+++ ariadne/point_net/loss.py
@@ -75,3 +75,46 @@ class PointNetImpulseLoss(nn.Module):
         impulse_pred = preds_all[:, :,  1:]
         impulse_tgt = target_all[:, :, 1:]
         return F.binary_cross_entropy(preds, target, weight=batch_weights) * 0.5 + torch.norm(impulse_pred - impulse_tgt) * 0.5
+
+
+@gin.configurable
+class PointNetClassificationLoss(nn.Module):
+    def __init__(self,
+                 real_weight=1,
+                 fake_weight=1,
+                 is_softmax=False,
+                 alpha=0.0001):
+        super().__init__()
+        self.real_weight = real_weight
+        self.fake_weight = fake_weight
+        self.alpha = alpha
+        if is_softmax:
+            self.criterion = F.cross_entropy
+        else:
+            self.criterion = F.binary_cross_entropy_with_logits
+
+    def _regularization_term(self, transform_matrix):
+        # identity matrix
+        id_transform = torch.eye(
+            transform_matrix.size(-1),
+            requires_grad=True
+        ).repeat(transform_matrix.size(0), 1, 1).to(transform_matrix.device)
+        return torch.norm(
+            id_transform - torch.bmm(transform_matrix, transform_matrix.transpose(1, 2))
+        )
+
+    def forward(self, preds, target):
+        # Compute target weights on-the-fly for loss function
+        batch_weights_real = target * self.real_weight
+        batch_weights_fake = (1 - target) * self.fake_weight
+        batch_weights = batch_weights_real + batch_weights_fake
+        # unpack predictions
+        preds, in_mtrx, feat_mtrx = preds
+        ce_loss_val = self.criterion(preds, target, weight=batch_weights)
+        reg_loss = 0
+        if in_mtrx is not None:
+            reg_loss += self._regularization_term(in_mtrx)
+        if feat_mtrx is not None:
+            reg_loss += self._regularization_term(feat_mtrx)
+        reg_loss = self.alpha * reg_loss / float(preds.size(0))
+        return ce_loss_val + reg_loss
--- ariadne/point_net/processor.py
+++ ariadne/point_net/processor.py
@@ -128,6 +128,25 @@ class PointNet_ProcessorBMN7(PointNet_Processor):
         chunk_df = chunk.df_chunk_data
         chunk_id = int(chunk_df.event.values[0])
         output_name = (self.output_dir + '/points_%s_%d' % (idx, chunk_id))
+        chunk_df = chunk_df[chunk_df.det == 1]
+
+        if not chunk_df[chunk_df.track < -1].empty:
+            logging.info("\nPointNet_Processor got hit with id < -1!\n for event %d" % chunk_id)
+            return TransformedPointsDataChunk(None, "")
+
+        if chunk_df[chunk_df.track == -1].empty:
+            logging.info("\nPointNet_Processor got event with no fakes!\n for event %d" % chunk_id)
+            return TransformedPointsDataChunk(None, "")
+
+        if chunk_df[chunk_df.track != -1].empty:
+            logging.info("\nPointNet_Processor got event with no real hits!\n for event %d" % chunk_id)
+            return TransformedPointsDataChunk(None, "")
+
+        if chunk_df.empty:
+            LOGGER.warning("SKIPPED empty %d event" % chunk_id)
+            return TransformedPointsDataChunk(None, output_name)
+
+
         for col in self.stats_cols:
             self.maxis[col] = max(chunk_df[col].max(), self.maxis[col])
             self.minis[col] = min(chunk_df[col].min(), self.minis[col])
