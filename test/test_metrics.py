073d0d6e4e2780ff030d77a4ccb2d2b4b78c9089
Pavel Goncharov <kaliostrogoblin3@gmail.com>
2021-02-21 05:03:08 +0300

    add script to test new metrics

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -1,13 +1,21 @@
+import gin
 import math
-import torch 
+import torch
 import unittest
 import numpy as np
 
 from unittest import TestCase
+import pytorch_lightning as pl
 from ariadne.tracknet_v2.metrics import (
     ellipse_area,
     point_in_ellipse,
     calc_metrics
+)
+from ariadne.metrics import (
+    accuracy,
+    f1_score,
+    recall,
+    precision
 )
 
 
@@ -32,7 +40,7 @@ class EllipseAreaTest(TestCase):
         for i in range(preds.size(0)):
             output = ellipse_area(preds[i:i+1])
             torch.testing.assert_allclose(output, expected_output[i])
-        
+
         output = ellipse_area(preds)
         torch.testing.assert_allclose(output, expected_output.mean())
 
@@ -70,7 +78,7 @@ class PointInEllipseTest(TestCase):
 
 class CalcMetricsTest(TestCase):
     def _create_model(self):
-        """Dummy model. Takes last x, y 
+        """Dummy model. Takes last x, y
         coords and predict ellipse with params
         (x, y, r1, r2) = (x+1, y+1, 1, 1)
         """
@@ -97,7 +105,7 @@ class CalcMetricsTest(TestCase):
         inputs = np.zeros((1, 2, 2))
         with self.assertRaisesRegex(ValueError, 'second dimension must be greater than 2'):
             calc_metrics(inputs, model)
-        
+
         inputs = np.zeros((1, 5, 3))
         with self.assertRaisesRegex(ValueError, 'Invalid tracklen, must be >2'):
             calc_metrics(inputs, model, tracklen=2)
@@ -125,8 +133,259 @@ class CalcMetricsTest(TestCase):
         _assert_output(output, expected_output)
 
 
+class TestClassificationMetrics(unittest.TestCase):
+    def setUp(self):
+        gin.clear_config()
+
+    @staticmethod
+    def etalon_recall(preds, target, threshold=0.5):
+        preds = TestClassificationMetrics._format_preds(preds, target, threshold)
+        result = preds[target == 1].float().mean()
+        if torch.isnan(result):
+            return torch.tensor(0.).float()
+        return result
+
+    @staticmethod
+    def _format_preds(preds, target, threshold):
+        if len(preds.shape) > len(target.shape):
+            # softmax output
+            preds = torch.argmax(preds, dim=1).bool()
+        else:
+            preds = preds > threshold
+        return preds
+
+    @staticmethod
+    def etalon_precision(preds, target, threshold=0.5):
+        preds = TestClassificationMetrics._format_preds(preds, target, threshold)
+        result = target[preds].float().mean()
+        if torch.isnan(result):
+            return torch.tensor(1.).float()
+        return result
+
+    @staticmethod
+    def etalon_accuracy(preds, target, threshold=0.5):
+        preds = TestClassificationMetrics._format_preds(preds, target, threshold)
+        return (preds == target).float().mean()
+
+    @staticmethod
+    def etalon_f1(preds, target, threshold=0.5):
+        recall = TestClassificationMetrics.etalon_recall(preds, target, threshold)
+        precision = TestClassificationMetrics.etalon_precision(preds, target, threshold)
+        return 2 * (precision * recall) / (precision + recall)
+
+    def _test_serial(self, target, test_inputs, metric_func):
+        if metric_func is recall:
+            etalon_func = self.etalon_recall
+        elif metric_func is precision:
+            etalon_func = self.etalon_precision
+        elif metric_func is accuracy:
+            etalon_func = self.etalon_accuracy
+        elif metric_func is f1_score:
+            etalon_func = self.etalon_f1
+        elif isinstance(metric_func, pl.metrics.Recall):
+            etalon_func = self.etalon_recall
+
+        for preds, result in test_inputs:
+            with self.subTest(preds=preds, target=target, result=result):
+                ariadne_result = metric_func(preds, target)
+                etalon_result = etalon_func(preds, target)
+                self.assertEqual(ariadne_result, etalon_result)
+                self.assertEqual(ariadne_result, result)
+
+    def test_recall(self):
+        # 1D case
+        target = torch.tensor([1, 1, 1, 1]).int()
+        test_inputs = [
+            (torch.tensor([0., 0., 0., 0.]), 0.),
+            (torch.tensor([0.6, 0.7, 0.99, 0.7]), 1.),
+            (torch.tensor([0., 0.6, 0.6, 0.]), 0.5),
+            (torch.tensor([0.2, 0.8, 0.45, 0.49]), 0.25)
+        ]
+        self._test_serial(target, test_inputs, recall)
+        # 2D case
+        target = target.view(2, 2)
+        test_inputs = [(preds.view(2, 2), result) for preds, result in test_inputs]
+        self._test_serial(target, test_inputs, recall)
+        # softmax case
+        test_inputs = [
+            (torch.tensor([
+                [[0.9, 0.1], [0.9, 0.1]],
+                [[0.8, 0.2], [0.9, 0.1]]
+            ]), 0.),
+            (torch.tensor([
+                [[0.2, 0.8], [0.3, 0.7]],
+                [[0.2, 0.8], [0.1, 0.9]]
+            ]), 1.),
+            (torch.tensor([
+                [[0.9, 0.1], [0.1, 0.9]],
+                [[0.2, 0.8], [0.6, 0.4]]
+            ]), 0.5),
+            (torch.tensor([
+                [[0.9, 0.1], [0.2, 0.8]],
+                [[0.7, 0.3], [0.6, 0.4]]
+            ]), 0.25),
+        ]
+        # (N, F, C) -> (N, C, F) classes are second
+        test_inputs = [(preds.transpose(2, 1), result) for preds, result in test_inputs]
+        gin.bind_parameter('ariadne.metrics.recall.is_softmax', True)
+        self._test_serial(target, test_inputs, recall)
+
+    def test_class_recall(self):
+        # 1D case
+        target = torch.tensor([1, 1, 1, 1]).int()
+        test_inputs = [
+            (torch.tensor([0., 0., 0., 0.]), 0.),
+            (torch.tensor([0.6, 0.7, 0.99, 0.7]), 1.),
+            (torch.tensor([0., 0.6, 0.6, 0.]), 0.5),
+            (torch.tensor([0.2, 0.8, 0.45, 0.49]), 0.25)
+        ]
+        recall_metric = pl.metrics.Recall(is_multiclass=False)
+        self._test_serial(target, test_inputs, recall_metric)
+        # 2D case
+        target = target.view(2, 2)
+        test_inputs = [(preds.view(2, 2), result) for preds, result in test_inputs]
+        self._test_serial(target, test_inputs, recall_metric)
+        # softmax case
+        test_inputs = [
+            (torch.tensor([
+                [[0.9, 0.1], [0.9, 0.1]],
+                [[0.8, 0.2], [0.9, 0.1]]
+            ]), 0.),
+            (torch.tensor([
+                [[0.2, 0.8], [0.3, 0.7]],
+                [[0.2, 0.8], [0.1, 0.9]]
+            ]), 1.),
+            (torch.tensor([
+                [[0.9, 0.1], [0.1, 0.9]],
+                [[0.2, 0.8], [0.6, 0.4]]
+            ]), 0.5),
+            (torch.tensor([
+                [[0.9, 0.1], [0.2, 0.8]],
+                [[0.7, 0.3], [0.6, 0.4]]
+            ]), 0.25),
+        ]
+        # (N, F, C) -> (N, C, F) classes are second
+        test_inputs = [(preds.transpose(2, 1), result) for preds, result in test_inputs]
+        recall_metric = pl.metrics.Recall(is_multiclass=True, mdmc_average='global')
+        gin.bind_parameter('ariadne.metrics.recall.is_softmax', True)
+        self._test_serial(target, test_inputs, recall_metric)
+
+    def test_precision(self):
+        # 1D case
+        target = torch.tensor([1, 0, 0, 0]).int()
+        test_inputs = [
+            (torch.tensor([0.2, 0.9, 0.4, 0.9]), 0.),
+            (torch.tensor([0.9, 0.0, 0.3, 0.4]), 1.),
+            (torch.tensor([0.9, 0.9, 0.2, 0.4]), 0.5),
+            (torch.tensor([0.9, 0.8, 0.7, 0.6]), 0.25),
+        ]
+        self._test_serial(target, test_inputs, precision)
+        # 2D case
+        target = target.view(2, 2)
+        test_inputs = [(preds.view(2, 2), result) for preds, result in test_inputs]
+        self._test_serial(target, test_inputs, precision)
+        # softmax case
+        test_inputs = [
+            (torch.tensor([
+                [[0.9, 0.1], [0.1, 0.9]],
+                [[0.8, 0.2], [0.1, 0.9]]
+            ]), 0.),
+            (torch.tensor([
+                [[0.2, 0.8], [0.7, 0.3]],
+                [[0.8, 0.2], [0.9, 0.1]]
+            ]), 1.),
+            (torch.tensor([
+                [[0.1, 0.9], [0.1, 0.9]],
+                [[0.8, 0.2], [0.6, 0.4]]
+            ]), 0.5),
+            (torch.tensor([
+                [[0.1, 0.9], [0.2, 0.8]],
+                [[0.3, 0.7], [0.4, 0.6]]
+            ]), 0.25),
+        ]
+        # (N, F, C) -> (N, C, F) classes are second
+        test_inputs = [(preds.transpose(2, 1), result) for preds, result in test_inputs]
+        gin.bind_parameter('ariadne.metrics.precision.is_softmax', True)
+        self._test_serial(target, test_inputs, precision)
+
+    def test_accuracy(self):
+        # 1D case
+        target = torch.tensor([1, 0, 0, 1]).int()
+        test_inputs = [
+            (torch.tensor([0.2, 0.9, 0.8, 0.1]), 0.),
+            (torch.tensor([0.9, 0.0, 0.3, 0.55]), 1.),
+            (torch.tensor([0.9, 0.2, 0.9, 0.4]), 0.5),
+            (torch.tensor([0.2, 0.2, 0.7, 0.49]), 0.25),
+        ]
+        self._test_serial(target, test_inputs, accuracy)
+        # 2D case
+        target = target.view(2, 2)
+        test_inputs = [(preds.view(2, 2), result) for preds, result in test_inputs]
+        self._test_serial(target, test_inputs, accuracy)
+        # softmax case
+        test_inputs = [
+            (torch.tensor([
+                [[0.9, 0.1], [0.1, 0.9]],
+                [[0.2, 0.8], [0.9, 0.1]]
+            ]), 0.),
+            (torch.tensor([
+                [[0.2, 0.8], [0.7, 0.3]],
+                [[0.8, 0.2], [0.1, 0.9]]
+            ]), 1.),
+            (torch.tensor([
+                [[0.1, 0.9], [0.9, 0.1]],
+                [[0.2, 0.8], [0.6, 0.4]]
+            ]), 0.5),
+            (torch.tensor([
+                [[0.9, 0.1], [0.8, 0.2]],
+                [[0.3, 0.7], [0.6, 0.4]]
+            ]), 0.25),
+        ]
+        # (N, F, C) -> (N, C, F) classes are second
+        test_inputs = [(preds.transpose(2, 1), result) for preds, result in test_inputs]
+        gin.bind_parameter('ariadne.metrics.accuracy.is_softmax', True)
+        self._test_serial(target, test_inputs, accuracy)
+
+    def test_f1(self):
+        # 1D case
+        # 1D case
+        target = torch.tensor([1, 1, 1, 1]).int()
+        # F1 = 2 * (precision * recall) / (precision + recall)
+        test_inputs = [
+            (torch.tensor([0., 0., 0., 0.]), 0.), # recall=0, precision=1, f1=0
+            (torch.tensor([0.6, 0.7, 0.99, 0.7]), 1.), # recall=1, precision=1, f1=1
+            (torch.tensor([0., 0.6, 0.6, 0.]), 0.66666666), # recall=0.5, precision=1, f1=0.666
+            (torch.tensor([0.2, 0.8, 0.45, 0.49]), 0.4) # recall=0.25, precision=1, f1=0.4
+        ]
+        self._test_serial(target, test_inputs, f1_score)
+        # 2D case
+        target = target.view(2, 2)
+        test_inputs = [(preds.view(2, 2), result) for preds, result in test_inputs]
+        self._test_serial(target, test_inputs, f1_score)
+        # softmax case
+        test_inputs = [
+            (torch.tensor([
+                [[0.9, 0.1], [0.9, 0.1]],
+                [[0.8, 0.2], [0.9, 0.1]]
+            ]), 0.),
+            (torch.tensor([
+                [[0.2, 0.8], [0.3, 0.7]],
+                [[0.2, 0.8], [0.1, 0.9]]
+            ]), 1.),
+            (torch.tensor([
+                [[0.9, 0.1], [0.1, 0.9]],
+                [[0.2, 0.8], [0.6, 0.4]]
+            ]), 0.66666666),
+            (torch.tensor([
+                [[0.9, 0.1], [0.2, 0.8]],
+                [[0.7, 0.3], [0.6, 0.4]]
+            ]), 0.4),
+        ]
+        # (N, F, C) -> (N, C, F) classes are second
+        test_inputs = [(preds.transpose(2, 1), result) for preds, result in test_inputs]
+        gin.bind_parameter('ariadne.metrics.f1_score.is_softmax', True)
+        self._test_serial(target, test_inputs, f1_score)
+
+
 if __name__ == '__main__':
     unittest.main()
-
-
-        
